16d43932127cb490346ea653131905763aba2efe
azkaBashir45 <azka.ngxoft@gmail.com>
2021-09-04 22:03:29 -0700

    next question

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,6 +1,7 @@
 import 'question.dart';
 
 class QuizBrain {
+  //ismy pehly store kry gy keh kis number pr h 
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
@@ -30,7 +31,7 @@ class QuizBrain {
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
   ];
-
+//create next question
   void nextQuestion() {
     if (_questionNumber < _questionBank.length - 1) {
       _questionNumber++;
